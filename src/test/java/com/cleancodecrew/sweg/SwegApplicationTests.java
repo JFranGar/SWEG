158270ffa3cde9782573c2a80df9cd4f4783e84c
Juan Francisco Chanatasig Garzón <jfchanatasig1405@gmail.com>
2026-05-14 18:07:06 -0500

    repository created

---
+++ src/test/java/com/cleancodecrew/sweg/SwegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancodecrew.sweg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
